c5696cb873c1b4e26ee1bf6289341cd1f9154e1e
MARBACH Andrew <andrew.marbach1@etu.univ-lorraine.fr>
2022-09-19 20:42:25 +0200

    Création Contact + Captcha

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +14,16 @@ class HomeController extends AbstractController
      * @Route("/", name="app_home")
      */
 
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+
+        $form=$this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
         return $this->render('home/index.html.twig', [
+            'form' => $form->createView(),
             'controller_name' => 'HomeController',
         ]);
     }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use VictorPrdh\RecaptchaBundle\Form\ReCaptchaType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+               
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+               
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                
+            ])
+            ->add('sujet', TextType::class, [
+                'label' => 'Sujet',
+                
+            ])
+            ->add('captcha',ReCaptchaType::class, [
+                'label' => 'Captcha',
+                
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+                'attr' => [
+                    'rows' => 10,
+                ],
+               
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
